5a8b94766999e88ab8a3d518b7f400a18adb48f1
kezhengjie <kzj961014@qq.com>
2021-08-04 15:13:52 +0800

    udpate

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 
 #include "util.h"
-#include "msg_queue.h"
+#include "msg_queue/msg_queue.h"
 
 #include "util.h"
 #include <map>
@@ -42,6 +42,7 @@ int main(int argc, char **argv)
 
 	msg_queue *mq = new msg_queue();
 	mq->register_msg(1, p);
+	mq->set_max_thread_num(1);
 	mq->start();
 
 	void *p;
