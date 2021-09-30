be4b77609d516f46f0df1393930f8176d4085480
kezhengjie <kzj961014@qq.com>
2021-09-30 17:47:58 +0800

    update demo main.cpp

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -13,13 +13,13 @@ int main(int argc, char **argv)
 	{
 		count++;
 		sprintf(buf, "this is test round %d", count);
-		//logutil::GetLogger("test")->Print(buf);
+		logutil::GetLogger("test")->Print(buf);
 		logutil::LogName("test").Print(buf);
 		sprintf(buf, "this is info round %d", count);
-		//logutil::GetLogger("info")->Print(buf);
+		logutil::GetLogger("info")->Print(buf);
 		logutil::LogName("info").Print(buf);
 		sprintf(buf, "this is error round %d", count);
-		//logutil::GetLogger("error")->Print(buf);
+		logutil::GetLogger("error")->Print(buf);
 		logutil::LogName("error").Print(buf);
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
